f2ef4ac96505f1bdb1badb5d0f8e2aea9ccf62b3
gbr759 <88782924+gbr759@users.noreply.github.com>
2024-12-05 11:14:24 +0000

    addvnet (#15)

---
+++ vnet.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "valheimrg" {
+  name     = var.valheim_rg_name
+  location = var.default_location
+}
+
+resource "azurerm_virtual_network" "valheimvnet" {
+  name                = "valheim-vnet"
+  resource_group_name = azurerm_resource_group.valheimrg.name
+  location            = azurerm_resource_group.valheimrg.location
+  address_space       = ["192.168.0.0/24"]
+}
+
+resource "azurerm_subnet" "valheimsubnet" {
+  name                 = "valheim-subnet"
+  resource_group_name  = azurerm_resource_group.valheimrg.name
+  virtual_network_name = azurerm_virtual_network.valheimvnet.name
+  address_prefixes     = ["192.168.0.0/26"]
+}
