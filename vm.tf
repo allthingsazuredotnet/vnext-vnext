2d12260af204425f31c205edb2bdaef1b5a8e915
gbr759 <88782924+gbr759@users.noreply.github.com>
2024-12-05 18:16:46 +0000

    fix (#17)

--- vm.tf
+++ vm.tf
@@ -15,7 +15,7 @@ resource "azurerm_virtual_machine" "example" {
   location              = azurerm_resource_group.valheimrg.location
   resource_group_name   = azurerm_resource_group.valheimrg.name
   network_interface_ids = [azurerm_network_interface.valheim.id]
-  vm_size               = "Standard_B2ms_v2"
+  vm_size               = "Standard_B2ms"
 
   storage_os_disk {
     name              = "valheimdisk"
