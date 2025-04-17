e7536ddcb7932175bffc965d6454596f6358e5cf
gbr759 <88782924+gbr759@users.noreply.github.com>
2025-04-17 15:35:41 +0100

    desploy asp (#26)

---
+++ appservice.tf
@@ -0,0 +1,18 @@
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_app_service_plan" "example" {
+  name                = "example-asp"
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+  sku {
+    tier     = "PremiumV3"
+    size     = "P0v3"
+    capacity = 1
+  }
+  per_site_scaling             = false
+  maximum_elastic_worker_count = 1
+  zone_redundant               = false
+}
