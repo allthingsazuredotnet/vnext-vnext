ae76cd7da6ef8a39820c74d3ed4304f63f83417e
gbr759 <chris.smallman@gmail.com>
2025-05-13 11:06:15 +0100

    fix

--- cae.tf
+++ cae.tf
@@ -36,8 +36,8 @@ resource "azurerm_key_vault" "kv" {
 
 resource "azurerm_log_analytics_workspace" "law" {
   name                = local.log_analytics_workspace_name
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.aiops_container.location
+  resource_group_name = azurerm_resource_group.aiops_container.name
   sku                 = "PerGB2018"
   retention_in_days   = 30
 
@@ -49,8 +49,8 @@ resource "azurerm_log_analytics_workspace" "law" {
 
 resource "azurerm_container_app_environment" "cae" {
   name                       = local.container_app_environment_name
-  location                   = azurerm_resource_group.rg.location
-  resource_group_name        = azurerm_resource_group.rg.name
+  location                   = azurerm_resource_group.aiops_container.location
+  resource_group_name        = azurerm_resource_group.aiops_container.name
   log_analytics_workspace_id = azurerm_log_analytics_workspace.law.id
 
   tags = {
