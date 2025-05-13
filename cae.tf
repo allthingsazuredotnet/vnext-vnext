9c90866e3fe6984b9af7a93ac5f41e9cbf4c716d
gbr759 <chris.smallman@gmail.com>
2025-05-13 17:00:00 +0100

    fix

--- cae.tf
+++ cae.tf
@@ -126,7 +126,7 @@ resource "azurerm_container_app" "aiops_payload" {
   secret {
     name = "my-kv-secret"
     #   key_vault_secret_id = azurerm_key_vault_secret.kv.id                               // Assuming you have a Key Vault secret resource
-    identity = azurerm_container_app.aiops_payload.identity[0].principal_id // Or specify a user-assigned identity
+    #   identity = azurerm_container_app.aiops_payload.identity[0].principal_id // Or specify a user-assigned identity
   }
 
   tags = {
