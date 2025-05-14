3879b88345b48df407ae9209ea40ad743a19bd6c
gbr759 <88782924+gbr759@users.noreply.github.com>
2025-05-14 16:13:38 +0100

    Upload (#62)
    * upload
    
    * add cae
    
    * fix
    
    * fix
    
    * add acr
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * updates
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * inside
    
    * formatting
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * uami
    
    * fix
    
    * ingress
    
    * formatting
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix
    
    * fix

--- cae.tf
+++ cae.tf
@@ -157,6 +157,7 @@ resource "azurerm_container_app" "aiops_payload" {
   ingress {
     target_port                = 5000   // Replace with the port your container application listens on
     transport                  = "http" // ACA handles TLS termination; your app receives HTTP traffic on target_port
+    external_enabled           = true   // Set to true to expose the app externally
     allow_insecure_connections = false  // Default is false, ensures HTTPS only
 
     traffic_weight {
